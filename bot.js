4e2561617d44a94c9e5492d8fef5bd915eedaa9e
mamaobama <94677479+mamaobama@users.noreply.github.com>
2026-05-15 12:38:28 +0300

    bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,16 @@
-console.log("Bot ran at:", new Date().toISOString());
+console.log("Bot started:", new Date());
+
+fetch("YOUR_RENEW_API_URL_HERE", {
+  method: "POST", // or GET depending on site
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer YOUR_TOKEN"
+  }
+})
+.then(res => res.text())
+.then(data => {
+  console.log("Renew result:", data);
+})
+.catch(err => {
+  console.log("Error:", err);
+});
